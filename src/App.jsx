cb6f25c0986a45317e0e4c5de8f13d9a3658fd5d
prpr <prpr@123.com>
2024-05-02 11:11:37 +0800

    fix app bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react'
-import { Skeleton, ImageList, ImageListItem, Box, Container, Paper, Card, CardContent, Typography, CardActions, Button, CardMedia, Collapse } from '@mui/material'
+import { Skeleton, ImageList, ImageListItem, Box, Container, AppBar, Toolbar, Card, CardContent, Typography, CardActions, Button, CardMedia, Collapse } from '@mui/material'
 import GitHubIcon from '@mui/icons-material/GitHub'
 import { MuiMarkdown, getOverrides } from 'mui-markdown'
 import { Highlight, themes } from 'prism-react-renderer'
@@ -24,47 +24,53 @@ function App() {
   }, [])
 
   return (
-    <Container maxWidth="xl">
-      <Box sx={{ p: 8 }}>
-        <ImageList cols={3} gap={32} variant='mansory'>
-          <ImageListItem>
-            <Card raised>
-              <CardMedia
-                sx={{ height: 400 }}
-                image={wallet}
-                title="Wallet"
-              />
-              <CardContent>
-                <Typography variant="h5">
-                  Ehtereum Wallet
-                </Typography>
-              </CardContent>
-              <CardActions>
-                <Button size="small" onClick={() => openTab('https://pangrr.github.io/wallet/')}>open wallet</Button>
-              </CardActions>
-            </Card>
-          </ImageListItem>
-          <ImageListItem>
-            <Card raised>
-              <CardContent>
-                <Collapse in={expandArticle1} collapsedSize={300}>
-                  <MuiMarkdown overrides={markdownOverrides}>{article1}</MuiMarkdown>
-                </Collapse>
-              </CardContent>
-              <CardActions>
-                <Button size="small" onClick={() => setExpandArticle1(!expandArticle1)}>{expandArticle1 ? 'collapse' : 'expand'}</Button>
-              </CardActions>
-            </Card>
-          </ImageListItem>
-          {[...Array(5).keys()].map((i) =>
-            <ImageListItem key={i}>
-              <Skeleton variant="rectangular" sx={{ bgcolor: 'grey.900' }} width={432} height={475} />
+    <>
+      <AppBar color='darkerBackground' enableColorOnDark>
+        <Toolbar variant="dense">
+          <Button onClick={() => window.open('https://github.com/pangrr/demo', '_blank')} startIcon={<GitHubIcon />} color='inherit'>source code</Button>
+        </Toolbar>
+      </AppBar>
+      <Container maxWidth="xl">
+        <Box sx={{ p: 8, maxHeight: '100%' }}>
+          <ImageList cols={3} gap={32} variant='mansory'>
+            <ImageListItem>
+              <Card raised>
+                <CardMedia
+                  sx={{ height: 400 }}
+                  image={wallet}
+                  title="Wallet"
+                />
+                <CardContent>
+                  <Typography variant="h5">
+                    Ehtereum Wallet
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <Button size="small" onClick={() => openTab('https://pangrr.github.io/wallet/')}>open wallet</Button>
+                </CardActions>
+              </Card>
             </ImageListItem>
-          )}
-        </ImageList>
-      </Box>
-      <Button onClick={() => window.open('https://github.com/pangrr/demo', '_blank')}  sx={{ position: 'fixed', top: 24, left: 32 }} startIcon={<GitHubIcon/>}>source code</Button>
-    </Container>
+            <ImageListItem>
+              <Card raised>
+                <CardContent>
+                  <Collapse in={expandArticle1} collapsedSize={300}>
+                    <MuiMarkdown overrides={markdownOverrides}>{article1}</MuiMarkdown>
+                  </Collapse>
+                </CardContent>
+                <CardActions>
+                  <Button size="small" onClick={() => setExpandArticle1(!expandArticle1)}>{expandArticle1 ? 'collapse' : 'expand'}</Button>
+                </CardActions>
+              </Card>
+            </ImageListItem>
+            {[...Array(5).keys()].map((i) =>
+              <ImageListItem key={i}>
+                <Skeleton variant="rectangular" sx={{ bgcolor: 'grey.900' }} width={432} height={475} />
+              </ImageListItem>
+            )}
+          </ImageList>
+        </Box>
+      </Container>
+    </>
   )
 
   function openTab(path) {
